271d4638577263bcb7c5ac45690db68000e50308
brminnick <brandon.r.minnick@gmail.com>
2016-08-29 15:21:26 -0700

    Updated BaseViewModel

--- EntryUITest/ViewModels/BaseViewModel.cs
+++ EntryUITest/ViewModels/BaseViewModel.cs
@@ -1,38 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
 namespace EntryUITest.ViewModels
 {
-    class BaseViewModel : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-        bool _busy;
+	class BaseViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+		bool _busy;
 
-        public bool IsBusy
-        {
-            get { return _busy; }
-            set
-            {
-                SetProperty(ref _busy, value);
-            }
-        }
+		public bool IsBusy
+		{
+			get { return _busy; }
+			set
+			{
+				SetProperty(ref _busy, value);
+			}
+		}
 
-        protected void SetProperty<T>(ref T backingStore, T value, [CallerMemberName] string propertyname = "", Action onChanged = null)
-        {
-            if (EqualityComparer<T>.Default.Equals(backingStore, value))
-                return;
+		protected void SetProperty<T>(ref T backingStore, T value, [CallerMemberName] string propertyname = "", Action onChanged = null)
+		{
+			if (EqualityComparer<T>.Default.Equals(backingStore, value))
+				return;
 
-            backingStore = value;
+			backingStore = value;
 
-            onChanged?.Invoke();
+			onChanged?.Invoke();
 
-            OnPropertyChanged(propertyname);
-        }
-        public void OnPropertyChanged(string name)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
-    }
+			OnPropertyChanged(propertyname);
+		}
+		public void OnPropertyChanged([CallerMemberName]string name = "")
+		{
+			var handle = PropertyChanged;
+			handle?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+	}
 }
