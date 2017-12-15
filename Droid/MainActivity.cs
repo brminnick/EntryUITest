0d128b7d8caaf03c3645f86c6f3a9f3d9edaeedb
brminnick <brandon.r.minnick@gmail.com>
2017-12-15 09:48:46 -0800

    Refactored MainActivity

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -7,14 +7,14 @@ namespace EntryUITest.Droid
 	[Activity(Label = "EntryUITest.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
-		protected override void OnCreate(Bundle bundle)
+		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			TabLayoutResource = Resource.Layout.Tabbar;
 			ToolbarResource = Resource.Layout.Toolbar;
 
-			base.OnCreate(bundle);
+			base.OnCreate(savedInstanceState);
 
-			global::Xamarin.Forms.Forms.Init(this, bundle);
+			global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
 			EntryCustomReturn.Forms.Plugin.Android.CustomReturnEntryRenderer.Init();
 
 			LoadApplication(new App());
