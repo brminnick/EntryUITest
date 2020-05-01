63f311f929382c9b465d5eb4664d194d0adcecb5
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-05-01 15:11:06 -0700

    Update NuGet Packages

--- EntryUITest/Pages/MainPage.cs
+++ EntryUITest/Pages/MainPage.cs
@@ -11,33 +11,29 @@ namespace EntryUITest.Pages
         {
             BackgroundColor = Color.FromHex("4FCAE6");
 
-            var goEntry = new Entry
-            {
-                ClearButtonVisibility = ClearButtonVisibility.WhileEditing,
-                Placeholder = "Enter Text Here",
-                PlaceholderColor = Color.FromHex("749FA8"),
-                AutomationId = AutomationIdConstants.EntryAutomationID,
-                TextColor = Color.FromHex("2C7797"),
-                BackgroundColor = Color.FromHex("91E2F4"),
-                ReturnType = ReturnType.Done,
-                ReturnCommand = new Command(Unfocus)
-            }.Bind(Entry.TextProperty, nameof(MainViewModel.EmailKeyboardEntryText));
-
-            var textLabel = new Label
-            {
-                TextColor = Color.White,
-                AutomationId = AutomationIdConstants.LabelAutomationID,
-                HorizontalTextAlignment = TextAlignment.Center
-            }.Bind(Label.TextProperty, nameof(MainViewModel.TextLabelText));
-
             Padding = GetPagePadding();
 
             Content = new StackLayout
             {
                 Children =
                 {
-                    goEntry,
-                    textLabel
+                    new Entry
+                    {
+                        ClearButtonVisibility = ClearButtonVisibility.WhileEditing,
+                        Placeholder = "Enter Text Here",
+                        PlaceholderColor = Color.FromHex("749FA8"),
+                        AutomationId = AutomationIdConstants.EntryAutomationID,
+                        TextColor = Color.FromHex("2C7797"),
+                        BackgroundColor = Color.FromHex("91E2F4"),
+                        ReturnType = ReturnType.Done,
+                        ReturnCommand = new Command(Unfocus)
+                    }.Bind(Entry.TextProperty, nameof(MainViewModel.EmailKeyboardEntryText)),
+                    new Label
+                    {
+                        TextColor = Color.White,
+                        AutomationId = AutomationIdConstants.LabelAutomationID,
+                        HorizontalTextAlignment = TextAlignment.Center
+                    }.Bind(Label.TextProperty, nameof(MainViewModel.TextLabelText))
                 }
             }.CenterExpand();
         }
